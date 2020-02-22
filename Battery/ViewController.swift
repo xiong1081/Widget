a51e8c78cfc56d589ed76d4423384b98ceeaa6d3
半熟木屋 <164508906@qq.com>
2020-02-22 15:29:09 +0800

    Initial Commit

---
+++ Battery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Battery
+//
+//  Created by 李招雄 on 2020/2/22.
+//  Copyright © 2020 李招雄. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
